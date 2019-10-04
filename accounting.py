90811e0988cb9469b6e2fa34c9f76be344549496
Alejandra Lopez <alemarlo1992@gmail.com>
2019-10-03 21:39:40 -0700

    Function, open file, and for loop

--- accounting.py
+++ accounting.py
@@ -6,53 +6,17 @@
 # Read over the solution and see how it compairs to your answer.
 
 
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
+def underpaid_melons(path):
 
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
+  melon_cost = 1.00
 
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
+  file = open(path)
 
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
+  for line in file: 
 
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
-        
+      if customer_expected != customer_paid:
 
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print(f"{customer2_name} paid ${customer2_paid:.2f},",
-          f"expected ${customer2_expected:.2f}"
-          )
+      print("{} paid ${},expected ${}".format(customer_name, customer_paid, 
+            customer_expected)
+            )
 
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print(f"{customer3_name} paid ${customer3_paid:.2f},",
-          f"expected ${customer3_expected:.2f}"
-          )
-
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print(f"{customer4_name} paid ${customer4_paid:.2f},",
-          f"expected ${customer4_expected:.2f}"
-          )
-
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print(f"{customer5_name} paid ${customer5_paid:.2f},",
-          f"expected ${customer5_expected:.2f}"
-          )
-
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print(f"{customer6_name} paid ${customer6_paid:.2f},",
-          f"expected ${customer6_expected:.2f}"
-          )
